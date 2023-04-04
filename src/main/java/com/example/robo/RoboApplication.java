37cf3ef2bd414e705095b62c4510edc114c9a44b
karthick.m <karthick.m@kgisl.com>
2023-04-04 09:22:16 +0530

    For Loops Hackerrank

--- src/main/java/com/example/robo/RoboApplication.java
+++ src/main/java/com/example/robo/RoboApplication.java
@@ -3,11 +3,18 @@ package com.example.robo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.robo.loops.Loops;
+
 @SpringBootApplication
 public class RoboApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RoboApplication.class, args);
+
+		// Hacker Rank For loop Questions
+		Loops hackerRankLoops = new Loops();
+
+		hackerRankLoops.forLoop();
 	}
 
 }
---
+++ src/main/java/com/example/robo/loops/Loops.java
@@ -0,0 +1,16 @@
+package com.example.robo.loops;
+
+public class Loops {
+    
+
+
+
+    public void forLoop(){
+
+        // Expected output -- 2 x 1 = 2
+        int N = 2;
+        for(int i=1; i<=10; i++){
+            System.out.println(N +" "+"x"+ " " + i+ " " + "=" + " " +N*i);
+        }
+    }
+}
