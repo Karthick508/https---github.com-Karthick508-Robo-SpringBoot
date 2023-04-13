7c4eec417e28dbb3688e1a6178761e022538ec31
karthick.m <karthick.m@kgisl.com>
2023-04-14 01:42:22 +0530

    Comments added.

--- src/main/java/com/example/robo/arrays/ShallowCopyDeepCopy.java
+++ src/main/java/com/example/robo/arrays/ShallowCopyDeepCopy.java
@@ -2,23 +2,27 @@ package com.example.robo.arrays;
 
 public class ShallowCopyDeepCopy extends Shallow {
 
-    // Shallow Copy
+    
+    /**
+     * ? Shallow Copy
+     * @throws CloneNotSupportedException
+     */
     public void shallowDeep() throws CloneNotSupportedException {
 
-        // Shallow copy heap memory have 1 object
+        // !Shallow copy heap memory have 1 object
         Shallow object = new Shallow(); // created Object
         object.i = 5;
         object.j = 6;
         System.out.println("Object " + object);
 
-        Shallow object1 = object; // here we created one object and two reference and object is same
+        Shallow object1 = object; // !here we created one object and two reference and object is same
         object1.i = 11;
         object1.j = 12;
         System.out.println("Object First " + object1);
         System.out.println("Object " + object);
 
-        // Deep Copy - creating 2 Objects and stored in 2 memory every we can give new
-        // value and it doesn't override
+        // ! Deep Copy - creating 2 Objects and stored in 2 memory every we can give new
+        // ! value and it doesn't override
         Deep deepObj = new Deep();
         deepObj.k = 22;
         deepObj.l = 23;
@@ -35,10 +39,10 @@ public class ShallowCopyDeepCopy extends Shallow {
         System.out.println("Deep Obj 1 " + deepObj1);
         System.out.println("Deep Obj " + deepObj);
 
-        // Cloning - creating 1 new Constructor and assign values to another object and
-        // it doesn't override and syntax like shallow copy and operation like deep copy
-        // basically it is protected type we need to implements and add an exception
-        // wherever we need to call this method
+        // !Cloning - creating 1 new Constructor and assign values to another object and
+        // ! it doesn't override and syntax like shallow copy and operation like deep copy
+        // ! basically it is protected type we need to implements and add an exception
+        // ! wherever we need to call this method
         Cloning cloneObj = new Cloning();
         cloneObj.m = 55;
         cloneObj.n = 66;
